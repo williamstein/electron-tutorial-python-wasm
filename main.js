18380c0e536a65cfa4dca771dbc4e6344d7550b4
William Stein <wstein@sagemath.com>
2022-11-19 08:33:45 -0800

    add preload

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 console.log("hello from main");
 
 const { app, BrowserWindow } = require("electron");
+const { join } = require("path");
 const debug = require("debug");
 const log = debug("main");
 
@@ -8,6 +9,9 @@ const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+      preload: join(__dirname, "preload.js"),
+    },
   });
 
   win.loadFile("index.html");
